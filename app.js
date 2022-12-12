201018fce908f0df421e7e05e8bb364fddf0b145
Mikolaj Juraczko <mikolaj@mikolajjuraczko.com>
2022-12-12 15:08:22 +0100

    Added the "Recently bought" functionality

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 "use strict";
 
+// Local storage setup
+if (!localStorage.getItem("toBuyListStored")) {
+  localStorage.setItem("toBuyListStored", JSON.stringify([]));
+}
+if (!localStorage.getItem("recentlyBoughtListStored")) {
+  localStorage.setItem("recentlyBoughtListStored", JSON.stringify([]));
+}
+
 // Selectors
 const addItemInputField = document.querySelector("#add-item-input-field");
 const addItemAisleSelect = document.querySelector("#add-item-aisle-select");
 const addItemButton = document.querySelector("#add-item-button");
 const toBuyAisleSelect = document.querySelector("#to-buy-aisle-select");
 const toBuyList = document.querySelector("#to-buy-list");
+const recentlyBoughtList = document.querySelector("#recently-bought-list");
 
 // Event listeners
 addItemButton.addEventListener("click", addItemToBuy);
 toBuyAisleSelect.addEventListener("click", toBuyAisleSort);
+toBuyList.addEventListener("click", toBuyCheckOff);
 
 // Functions
 function addItemToBuy(e) {
@@ -22,25 +32,24 @@ function addItemToBuy(e) {
   };
 
   // Store the element in the local storage
-  let toBuyListStored;
-  if (!localStorage.getItem("toBuyListStored")) {
-    toBuyListStored = [];
-  } else {
-    toBuyListStored = JSON.parse(localStorage.getItem("toBuyListStored"));
-  }
+  let toBuyListStored = JSON.parse(localStorage.getItem("toBuyListStored"));
   toBuyListStored.push(toBuyElementObject);
   localStorage.setItem("toBuyListStored", JSON.stringify(toBuyListStored));
 
-  // Add the element to the page
+  // Add the element to the "To buy" list
   const toBuyElementLi = document.createElement("li");
   toBuyElementLi.classList.add(toBuyElementObject.elementAisle);
   const toBuyElementP = document.createElement("p");
   const toBuyElementButton = document.createElement("button");
+  toBuyElementButton.classList.add("check-mark-button");
   toBuyElementButton.innerText = "Done";
   toBuyElementP.innerHTML = `${toBuyElementObject.elementName} <span class="aisle">(${toBuyElementObject.elementAisle})</span>`;
   toBuyElementLi.appendChild(toBuyElementP);
   toBuyElementLi.appendChild(toBuyElementButton);
   toBuyList.appendChild(toBuyElementLi);
+
+  // Clear the input field
+  addItemInputField.value = "";
 }
 
 function toBuyAisleSort(e) {
@@ -115,4 +124,54 @@ function toBuyAisleSort(e) {
         break;
     }
   });
+}
+
+function toBuyCheckOff(e) {
+  if (e.target.classList.contains("check-mark-button")) {
+    // Get the element name
+    const toBuyListElement = e.target.parentElement;
+    const elementName = toBuyListElement.children[0].innerText.split(" ")[0];
+
+    // Get the stored lists
+    let toBuyListStored = JSON.parse(localStorage.getItem("toBuyListStored"));
+    let recentlyBoughtListStored = JSON.parse(
+      localStorage.getItem("recentlyBoughtListStored")
+    );
+
+    toBuyListStored.every((item) => {
+      // Add the element to the "Recently Bought" stored list:
+      if (item.elementName === elementName) {
+        recentlyBoughtListStored.push(item);
+        if (recentlyBoughtListStored.length > 5) {
+          recentlyBoughtList.childNodes[
+            recentlyBoughtList.childNodes.length - 1
+          ].remove();
+          recentlyBoughtListStored.shift();
+        }
+        localStorage.setItem(
+          "recentlyBoughtListStored",
+          JSON.stringify(recentlyBoughtListStored)
+        );
+        // Add the element to the "Recently bought" list
+        const toBuyElementLi = document.createElement("li");
+        toBuyElementLi.classList.add(item.elementAisle);
+        const toBuyElementP = document.createElement("p");
+        toBuyElementP.innerHTML = `${item.elementName} <span class="aisle">(${item.elementAisle})</span>`;
+        toBuyElementLi.appendChild(toBuyElementP);
+        recentlyBoughtList.prepend(toBuyElementLi);
+
+        // Remove the element from the "To buy" stored list
+        toBuyListStored.splice(toBuyListStored.indexOf(item), 1);
+        localStorage.setItem(
+          "toBuyListStored",
+          JSON.stringify(toBuyListStored)
+        );
+        return false;
+      }
+      return true;
+    });
+
+    // Remove the element from the list
+    toBuyListElement.remove();
+  }
 }
